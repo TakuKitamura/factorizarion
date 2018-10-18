03446384b8bbcda731aad7cf4514fa938b167dce
TakuKitamura <tikutaku0128@icloud.com>
2018-10-18 23:35:46 +0900

    finish

---
+++ src/factorization/bigint_factorization.go
@@ -0,0 +1,131 @@
+package factorization
+
+import (
+	"errors"
+	"math/big"
+)
+
+func BigintIsPrime(n big.Int) bool {
+	tempBigInt := big.NewInt(0)
+
+	if n.Cmp(big.NewInt(2)) == -1 { // n < 2
+		return false
+	} else if n.Cmp(big.NewInt(2)) == 0 { //n == 2
+		return true
+	} else if big.NewInt(0).Cmp(tempBigInt.Mod(&n, big.NewInt(2))) == 0 { // n % 2 == 0
+		return false
+	}
+
+	sn := new(big.Int).Sqrt(&n)
+
+	for i := big.NewInt(3); i.Cmp(sn) != 1; i = i.Add(i, big.NewInt(2)) { // i = 3; i <= sn; i+=2
+		if big.NewInt(0).Cmp(tempBigInt.Mod(&n, i)) == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func BigintGcd(m big.Int, n big.Int) *big.Int {
+	x := new(big.Int)
+	y := new(big.Int)
+	z := new(big.Int)
+	return z.GCD(x, y, &m, &n)
+}
+
+func BigintPollardsRho(n big.Int) *big.Int {
+	if n.Cmp(&n) == 1 { //n == 1
+		return big.NewInt(1)
+	}
+
+	x := big.NewInt(2)
+	y := big.NewInt(2)
+	d := big.NewInt(1)
+
+	f := func(x *big.Int) *big.Int {
+		xx := x.Mul(x, x)
+		xxPlusOne := xx.Add(xx, big.NewInt(1))
+		return xxPlusOne.Mod(xxPlusOne, &n)
+	}
+
+	for d.Cmp(big.NewInt(1)) == 0 {
+		x = f(x)
+		y = f(f(y))
+		z := big.NewInt(0)
+
+		if x.Cmp(y) == 1 { // x > y
+			z.Sub(x, y)
+		} else {
+			z.Sub(y, x)
+		}
+		d = BigintGcd(*z, n)
+	}
+
+	if d == &n {
+		return big.NewInt(0)
+	}
+
+	return d
+
+}
+
+func BigintTrialDivision(n big.Int) *big.Int {
+
+	x := *new(big.Int).Set(&n)
+
+	tempBigInt := big.NewInt(0)
+	d := big.NewInt(0)
+	q := big.NewInt(0)
+
+	for x.Cmp(big.NewInt(3)) == 1 && big.NewInt(0).Cmp(tempBigInt.Mod(&x, big.NewInt(2))) == 0 { // x > 3 && x%2 == 0
+		return big.NewInt(2)
+	}
+
+	d = big.NewInt(3)
+
+	q.Div(&x, d)
+
+	for q.Cmp(d) != -1 { //q >= d
+		if big.NewInt(0).Cmp(tempBigInt.Mod(&x, d)) == 0 { //x%d == 0
+			return d
+		} else {
+			d.Add(d, big.NewInt(2))
+		}
+		q.Div(&x, d)
+	}
+
+	return &x
+}
+
+func BigintMain(n big.Int) (string, error) {
+
+	if n.Cmp(big.NewInt(1)) == -1 { //n < 1
+		err := errors.New("should be args[1] > 0.")
+		return "", err
+	}
+
+	text := "Factorization: " + n.String() + " = 1 * "
+
+	for {
+		d := BigintPollardsRho(n)
+		if d.Cmp(big.NewInt(2)) == -1 { // d < 2
+			if BigintIsPrime(n) == true || n.Cmp(big.NewInt(1)) == 0 {
+				text += n.String() + " * "
+				break
+			} else {
+				d = BigintTrialDivision(n)
+			}
+		} else {
+			if BigintIsPrime(*d) == false {
+				d = BigintTrialDivision(*d)
+			}
+		}
+		text += d.String() + " * "
+		n.Div(&n, d)
+	}
+
+	textLength := len(text) - 3
+	formula := text[:textLength]
+
+	return formula, nil
+}
---
+++ src/factorization/uint_factorization.go
@@ -0,0 +1,130 @@
+package factorization
+
+import (
+	"errors"
+	"math"
+	"math/big"
+	"strconv"
+)
+
+func UintIsPrime(n uint64) bool {
+	if n < 2 {
+		return false
+	} else if n == 2 {
+		return true
+	} else if n%2 == 0 {
+		return false
+	} else {
+		// pass
+	}
+
+	sn := math.Sqrt(float64(n))
+
+	for i := uint64(3); float64(i) <= sn; i += 2 {
+		if n%i == 0 {
+			return false
+		}
+	}
+
+	return true
+
+}
+
+func UintGcd(m uint64, n uint64) uint64 {
+	x := new(big.Int)
+	y := new(big.Int)
+	z := new(big.Int)
+	a := new(big.Int).SetUint64(m)
+	b := new(big.Int).SetUint64(n)
+	return z.GCD(x, y, a, b).Uint64()
+}
+
+func UintPollardsRho(n uint64) uint64 {
+	if n == 1 {
+		return 1
+	}
+
+	x := uint64(2)
+	y := uint64(2)
+	d := uint64(1)
+
+	f := func(x uint64) uint64 {
+		return (x*x + 1) % n
+	}
+	// i := uint64(0)
+	// limit := uint64(math.Pow(float64(n), 0.05))
+	// for d == 1 && i < limit {
+	x = f(x)
+	y = f(f(y))
+
+	z := uint64(0)
+	if x > y {
+		z = x - y
+	} else {
+		z = y - x
+	}
+
+	d = UintGcd(z, n)
+	// 	i += 1
+	// }
+	// fmt.Println(limit, i, 123)
+
+	if d == n {
+		return uint64(0)
+	}
+
+	return d
+
+}
+
+func UintTrialDivision(x uint64) uint64 {
+	for x >= 4 && x%2 == 0 {
+		return 2
+	}
+
+	d := uint64(3)
+	q := x / d
+
+	for q >= d {
+		if x%d == 0 {
+			return d
+		}
+		d += 2
+		q = x / d
+	}
+
+	return x
+}
+
+func UintMain(n uint64) (string, error) {
+
+	if n < 1 {
+		err := errors.New("should be args[1] > 0.")
+		return "", err
+	}
+
+	text := "Factorization: " + strconv.FormatUint(n, 10) + " = 1 * "
+
+	for {
+		d := UintPollardsRho(n)
+		if d < 2 {
+			if UintIsPrime(n) == true || n == 1 {
+				text += strconv.FormatUint(n, 10) + " * "
+				break
+			} else {
+				d = UintTrialDivision(n)
+			}
+		} else {
+			if UintIsPrime(d) == false {
+				d = UintTrialDivision(d)
+			}
+		}
+		text += strconv.FormatUint(d, 10) + " * "
+		n = n / d
+
+	}
+	textLength := len(text) - 3
+	formula := text[:textLength]
+
+	return formula, nil
+}
